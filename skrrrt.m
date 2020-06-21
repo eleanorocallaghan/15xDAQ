ad4a3e38b0e636a4de62c343c444d514163edd76
eleanorocallaghan <eleanor.ocallaghan@gmail.com>
2020-06-21 11:45:03 -0400

    khxkh

--- skrrrt.m
+++ skrrrt.m
@@ -5,6 +5,6 @@ function [carSpeed,topSpeed,speed,maxSpeed]=skrrrt(hallEffect, sog)
 carSpeed = hallEffect*(1/55)*(20*pi)*60*60*(1/63360);
 topSpeed = max(carSpeed);
 %from GPS
-speed=sog/1.61;
-maxSpeed=max(speed);
+speed = sog/1.61;
+maxSpeed = max(speed);
 end
